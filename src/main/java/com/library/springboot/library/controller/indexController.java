ce46d2a88fb0c838d2c3c91b575edf6b83f7498f
Dongyeong Kim <kjptts2241@gmail.com>
2022-09-24 23:19:11 +0900

    환경설정 및 토대 구성

---
+++ src/main/java/com/library/springboot/library/controller/indexController.java
@@ -0,0 +1,36 @@
+package com.library.springboot.library.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class indexController {
+    
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/mybook")
+    public String mybook() {
+        return "mybook";
+    }
+
+    @GetMapping("/map")
+    public String map() {
+        return "map";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+}
