4dcec6210c0fc7be02d82cb9884b2275c55449be
Dongyeong Kim <kjptts2241@gmail.com>
2022-10-03 22:01:29 +0900

    api 및css 수정

--- src/main/java/com/library/springboot/library/controller/indexController.java
+++ src/main/java/com/library/springboot/library/controller/indexController.java
@@ -2,40 +2,42 @@ package com.library.springboot.library.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import groovyjarjarpicocli.CommandLine.Model;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
 public class indexController {
     
-    @GetMapping({"", "/"})
-    public String index() {
+    @GetMapping({"", "/"}) // http://localhost:8080, http://localhost:8080/
+    public String indexView() {
         return "index";
     }
 
-    @GetMapping("/mybook")
-    public String mybook() {
+    @GetMapping("/mybook")   // http://localhost:8080/mybook
+    public String mybookView() {
         return "mybook";
     }
 
-    @GetMapping("/map")
-    public String map() {
+    @GetMapping("/map")  // @PostMapping 전달 
+    public String mapView() {
         return "map";
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String loginView() {
         return "login";
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String registerView() {
         return "register";
     }
 
     @GetMapping("/search")
-    public String search() {
+    public String searchView() {
         return "search";
     }
 }
