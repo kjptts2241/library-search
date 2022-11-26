a47ffd5bc51ad2680e90e5ee3eb8df798a037b7a
DongYoung Kim <55075836+kjptts2241@users.noreply.github.com>
2022-11-26 23:04:08 +0900

    회원가입 이메일 인증 기능 추가 중

--- src/main/java/com/library/springboot/library/controller/RegController.java
+++ src/main/java/com/library/springboot/library/controller/RegController.java
@@ -1,11 +1,15 @@
 package com.library.springboot.library.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.library.springboot.library.service.UserMailSendService;
 import com.library.springboot.library.service.UserRegService;
 import com.library.springboot.library.service.UserSha256;
 import com.library.springboot.library.vo.UserVO;
@@ -16,13 +20,14 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class RegController {
     
+	private UserMailSendService mailsender;
     private final UserRegService reg_service;
 
     /*
     회원가입
      */
     @PostMapping("/signUp")
-    public String userRegPass(UserVO userVO) {
+    public String userRegPass(UserVO userVO, Model model, HttpServletRequest request) {
         
         
 		// 비밀번호 암호화(sha256)
@@ -31,6 +36,9 @@ public class RegController {
 
         // 회원가입 메서드
         reg_service.userReg_service(userVO);
+
+        // 인증 메일 보내기 메서드
+		mailsender.mailSendWithUserKey(userVO.getUser_email(), userVO.getUser_id(), request);
 
         return "redirect:login";
     }
